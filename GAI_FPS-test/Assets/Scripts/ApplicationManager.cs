81ec07a658dafc9997363749714dc0532cfac171
scottlooney <scottrlooney@gmail.com>
2015-06-19 03:13:19 -0700

    update - added preliminary Intro text Scroll window, plus transition to headbanging intro. still need position and camera animation change from ships head to facing the exit door.

--- GAI_FPS-test/Assets/Scripts/ApplicationManager.cs
+++ GAI_FPS-test/Assets/Scripts/ApplicationManager.cs
@@ -7,11 +7,25 @@ public class ApplicationManager : MonoBehaviour {
 	public GameObject ScreenLock;
 	bool NewBtn1x=false;
 	public Button ContBtn;
+	public Animator Scrolltext;
+	public Animator MainMenu;
+	public GameObject ScrollText;
+	public string[] TextBoxTexts;
+	public GameObject background;
+	public Color white;
+	public Color faded;
+	public float fadeTime;
+	public GameObject floatingStars;
+	float t=0.0f;
+	int count=0;
+	public bool startfade = false;
+
 
 	void Start()
 	{
 	//set up Continue Button as disabled
-		ContBtn.interactable=false;	
+		ContBtn.interactable=false;
+		Scrolltext.SetBool("Open",false);	
 	}
 
 	public void Quit () 
@@ -27,15 +41,54 @@ public class ApplicationManager : MonoBehaviour {
 	public void NewGame ()
 	{
 		if (NewBtn1x==false){
-			Application.LoadLevelAdditive("Graybox-testAI-legacy");
-			StartCoroutine("delayedUI_sceneLoad");
-			ContBtn.interactable=true;
+			MainMenu.SetBool("Open", false);
+			Scrolltext.SetBool("Open",true);
 			NewBtn1x=true;
 		}else{
 			//probably some sort of popup dialog asking if you're sure you want a New Game
 			//ContBtnEnable=false - until New game starts (probably on confirm of New Game)
 		}	
 	}
+
+	public void NextScene(){
+	
+		count++;
+		switch (count){
+			case 1:
+			//start transition of background fading out to expose ship's head environment w/ bobbing head
+			startfade=true;
+			//muffled background audio of assault with FPS humming and tinny music playing
+			//change text box
+				ScrollText.GetComponent<Text>().text = TextBoxTexts[0];
+			break;
+			case 2:
+			//change audio to quizzical while alarm is flashing 
+			//change text box
+				ScrollText.GetComponent<Text>().text = TextBoxTexts[1];
+			break;
+			case 3:
+				if (NewBtn1x){
+				Application.LoadLevelAdditive("Graybox-testAI-legacy");
+				StartCoroutine("delayedUI_sceneLoad");
+				ContBtn.interactable=true;	
+		}
+				
+			break;
+		}
+	}
+
+
+	public void StartGame()
+	{
+		if (NewBtn1x){
+			Application.LoadLevelAdditive("Graybox-testAI-legacy");
+			StartCoroutine("delayedUI_sceneLoad");
+			ContBtn.interactable=true;	
+		}
+	
+	}
+
+
 
 	public void Continue()
 	{
@@ -44,6 +97,22 @@ public class ApplicationManager : MonoBehaviour {
 		Debug.Log("Continue Btn Pressed!");
 		ScreenLock.SendMessage("ContinueBtn");
 		
+	}
+
+	void Update(){
+		if(startfade)
+		{
+			Color fading = Color.Lerp(Color.white, faded, t);
+			background.GetComponent<SpriteRenderer>().color = fading;
+			if (t<1)
+				t += Time.deltaTime/fadeTime;
+			else
+				startfade=false;
+				floatingStars.SetActive(false);
+			Debug.Log(fading);
+		}
+				
+
 	}
 
 	IEnumerator delayedUI_sceneLoad()
