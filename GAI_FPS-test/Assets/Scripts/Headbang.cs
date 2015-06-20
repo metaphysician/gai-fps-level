4331b0611caeb7bc9ef093da0fb14976133640c5
Alszerjo <alszerjo@gmail.com>
2015-06-20 13:35:55 -0700

    Preliminary Headbanging animation and a couple of test doors for approval.

---
+++ GAI_FPS-test/Assets/Scripts/Headbang.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Headbang : MonoBehaviour {
+	public double BPM;
+	public double bangDist;
+	public double whiplash;
+	private double phase;
+	private double twoPi;
+	private double w1;
+	private double w2;
+
+	void Start () {
+		twoPi = 2 * Math.PI;
+		if (whiplash > 1) {
+			whiplash = 1;
+		} else if (whiplash < 0) {
+			whiplash = 0;
+		}
+		w1 = Math.PI + (Math.PI * whiplash);
+		w2 = Math.PI - (Math.PI * whiplash);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.rotation = Quaternion.Euler((float)((1-AsyncCos (phase))*(-1)*bangDist), 40f, 0f);
+		phase += BPM/60*twoPi*Time.deltaTime;
+		if (phase >= twoPi) {
+			phase -= twoPi;
+		}
+	}
+	
+	double AsyncCos(double xp){
+		if (xp <= w1) {
+			return (-Math.Cos(xp*Math.PI/w1)+1)/2;
+		} else {
+			return (-Math.Cos(xp*Math.PI/w2)+1)/2;
+		}
+	}
+}
---
+++ GAI_FPS-test/Assets/SingleDoor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleDoor : MonoBehaviour {
+	private float baseZ;
+	public float Size;
+	public float Speed;
+	public GameObject Door;
+
+	void Start () {
+		baseZ = Door.transform.localPosition.z;
+	}
+	
+	void OnTriggerEnter(){
+		StopAllCoroutines ();
+		StartCoroutine (Open ());
+	}
+	
+	void OnTriggerExit(){
+		StopAllCoroutines ();
+		StartCoroutine (Close ());
+	}
+	
+	private IEnumerator Open(){
+		while (Door.transform.localPosition.z > -Size) {
+			Door.transform.Translate(new Vector3(0f,0f,-Speed*Time.deltaTime));
+			yield return null;
+		}
+	}
+	
+	private IEnumerator Close(){
+		while (Door.transform.localPosition.z < baseZ) {
+			Door.transform.Translate(new Vector3(0f,0f,Speed*Time.deltaTime));
+			yield return null;
+		}
+	}
+}
